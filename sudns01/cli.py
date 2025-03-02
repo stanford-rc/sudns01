6135dc12cae7a9047512fb5603cdd73798dd1d15
A. Karl Kornel <akkornel@stanford.edu>
2025-03-02 01:33:04 -0800

    cli: Split Stanford and non-Stanford
    Create separate main entrypoints for Stanford and non-Stanford ACME
    servers.  This is a lot of churn, because the shared functionality is
    placed into its own function.
    
    This also changes argument parsing to have shared options in a parent
    parser, with separate Stanford and non-Stanford child parsers.

--- sudns01/cli.py
+++ sudns01/cli.py
@@ -71,6 +71,7 @@ import binascii
 import logging
 import pathlib
 import sys
+from typing import NoReturn
 
 # PyPi imports
 import dns.message
@@ -84,32 +85,20 @@ import sudns01.clients.resolver
 import sudns01.clients.tkey
 import sudns01.clients.exceptions
 
-# Process command-line arguments
+# Make a generic argument parser
 argp = argparse.ArgumentParser(
-    prog='Stanford ACME DNS Updater',
+	add_help=False,
+	description='GSS-TSIG ACME DNS Updater',
     epilog=(
         'Your GSSAPI does ' + 
         ('' if sudns01.clients.tkey.HAS_CREDENTIAL_STORE else 'not ') +
         'support the Credential Store Extensions.'
     )
 )
-argp.add_argument('--nsupdate',
-    help='The DNS server that handles nsupdate messages.',
-    default='acme-dns.stanford.edu',
-)
-argp.add_argument('--port',
-    help='The port to use on the nsupdate server.',
-    type=int,
-    default=53,
-)
 argp.add_argument('--timeout',
     help='How long to wait for a response from the DNS servers.',
     type=float,
     default=10.0,
-)
-argp.add_argument('--udp',
-    help='Force using UDP for DNS queries.  Should never be needed.',
-    action='store_true',
 )
 argc = argp.add_mutually_exclusive_group()
 argc.add_argument('--cleanup',
@@ -142,17 +131,8 @@ argp.add_argument('name',
 argp.add_argument('challenge',
     help='The ACME DNS01 challenge string.',
 )
-args = argp.parse_args()
 
-# Set up logging
-logging.basicConfig(
-    level=(
-        'DEBUG' if args.debug is True else (
-            'INFO' if args.verbose is True else 'WARNING'
-        )
-    ),
-)
-
+# Partially set up logging
 logger = logging.getLogger(__name__)
 exception = logger.exception
 error = logger.error
@@ -160,198 +140,261 @@ warn = logger.warning
 info = logger.info
 debug = logger.debug
 
-# Are we doing a cleanup?
-cleanup_challenge = "{:#010x}".format(
-    binascii.crc32(args.name.encode('ASCII'))
-)
-if args.cleanup:
-    # Tell the user what challenge to provide
-    print('This option will remove other ACME Challenge TXT records for {args.name}.')
-    print('THIS CAN BE DANGEROUS!  The TXT records might be in place for other reasons.')
-    print(f"To cleanup anyway, change `--cleanup` to `--cleanup2 {cleanup_challenge}` and try again.")
-    sys.exit(1)
-if args.cleanup2 is not None:
-    # The user has set --cleanup2.  What challenge do we expect?
-    debug(f"Found cleanup, expecting challenge {cleanup_challenge}")
+# Handle running as a generic ACME GSS-TSIG command
+def main_generic() -> NoReturn:
+	# Make an argument parser for the generic command
+	argp_generic = argparse.ArgumentParser(
+		parents=[argp],
+	)
+	argp_generic.add_argument('--port',
+		help='The port to use on the nsupdate server.',
+		type=int,
+		default=53,
+	)
+	argp_generic.add_argument('--udp',
+		help='Force using UDP for DNS queries.  Should never be needed.',
+		action='store_true',
+	)
+	argp_generic.add_argument('nsupdate',
+		help='The DNS server that handles nsupdate messages.',
+	)
 
-    # If the user provided a challenge, and it does _not_ match, then tell them.
-    if args.cleanup2 != cleanup_challenge:
-        print(f"You provided the wrong cleanup challenge for {args.name}.")
-        print(f"Change `--cleanup2 {args.cleanup2}` to `--cleanup` and try again.")
-        sys.exit(1)
-    else:
-        # We got a valid challenge!
-        info('Will cleanup other ACME challenges')
+	# Now do the actual stuff!
+	main_common(argp_generic.parse_args())
+	
+# Handle running as sudns01
+def main_stanford() -> NoReturn:
+	# Make an argument parser for the Stanford command
+	argp_stanford = argparse.ArgumentParser(
+		parents=[argp],
+		description='Stanford NetDB ACME DNS Updater',
+	)
+	argp_stanford.add_argument('--nsupdate',
+		help=argparse.SUPPRESS,
+		default='acme-dns.stanford.edu',
+	)
+	argp_stanford.add_argument('--port',
+		help=argparse.SUPPRESS,
+		type=int,
+		default=53,
+	)
+	argp_stanford.add_argument('--udp',
+		help=argparse.SUPPRESS,
+		action='store_true',
+	)
 
-# Parse custom Kerberos credentials config.
-CREDS: sudns01.clients.tkey.KrbCreds | None = None
-if sudns01.clients.tkey.HAS_CREDENTIAL_STORE:
-    debug('We have the Credential Store Extensions')
-    if args.ccache is not None or args.keytab is not None:
-        CREDS = sudns01.clients.tkey.KrbCreds(
-            ccache=args.ccache,
-            client_keytab=args.keytab,
-        )
+	# Now do the actual stuff!
+	main_common(argp_stanford.parse_args())
 
-DNSUPDATE_SERVER: str = args.nsupdate
-DNSUPDATE_PORT: int = args.port
-DNSUPDATE_TIMEOUT: float = args.timeout
-TARGET_NAME: dns.name.Name = dns.name.from_text(args.name)
-TTL: int = 60
-CHALLENGE: str = args.challenge
-dns_queries_on_udp = args.udp
+# Do the common work!
+def main_common(
+    args: argparse.Namespace,
+) -> NoReturn:
+	# Do top-level (program-level) logging configuration
+	logging.basicConfig(
+		level=(
+			'DEBUG' if args.debug is True else (
+				'INFO' if args.verbose is True else 'WARNING'
+			)
+		),
+	)
+	
+	# Are we doing a cleanup?
+	cleanup_challenge = "{:#010x}".format(
+		binascii.crc32(args.name.encode('ASCII'))
+	)
+	if args.cleanup:
+		# Tell the user what challenge to provide
+		print('This option will remove other ACME Challenge TXT records for {args.name}.')
+		print('THIS CAN BE DANGEROUS!  The TXT records might be in place for other reasons.')
+		print(f"To cleanup anyway, change `--cleanup` to `--cleanup2 {cleanup_challenge}` and try again.")
+		sys.exit(1)
+	if args.cleanup2 is not None:
+		# The user has set --cleanup2.  What challenge do we expect?
+		debug(f"Found cleanup, expecting challenge {cleanup_challenge}")
 
-# Set up a Resolver
-dnslookup = sudns01.clients.resolver.ResolverClient()
-
-# From the target name (a FQDN), we need to work out:
-# * The _acme-challenge FQDN
-# * The underlying zone name
-# * The _acme-challenge name relative to the zone name
-target_domain = dnslookup.get_zone_name(TARGET_NAME)
-ACME_CHALLENGE_LABEL = dns.name.Name(labels=('_acme-challenge',))
-acme_challenge_name = ACME_CHALLENGE_LABEL.concatenate(TARGET_NAME)
-acme_challenge_name_relative = acme_challenge_name.relativize(target_domain)
-info(f"We will be working in domain {target_domain}")
-info(f"We will be modifying label {acme_challenge_name_relative}")
-
-# Get the IPs for our DNS server
-try:
-    dnsupdate_server_ips = dnslookup.get_ip(DNSUPDATE_SERVER)
-except sudns01.clients.exceptions.ResolverError as e:
-    print(f"Temporary error looking up {DNSUPDATE_SERVER}: {e}")
-    sys.exit(2)
-except sudns01.clients.exceptions.ResolverErrorPermanent as e:
-    print(f"Permanent error looking up {DNSUPDATE_SERVER}: {e}")
-    sys.exit(1)
-if len(dnsupdate_server_ips) == 0:
-    print(f"No IP addresses found for {DNSUPDATE_SERVER}")
-    sys.exit(1)
-
-# Create the client for sending DNS queries
-dnsquery = sudns01.clients.query.QueryClient(
-    ips=dnsupdate_server_ips,
-    port=DNSUPDATE_PORT,
-    timeout=DNSUPDATE_TIMEOUT,
-    udp=dns_queries_on_udp,
-)
-
-# Set up DNS signing
-try:
-    signer = sudns01.clients.tkey.GSSTSig(
-        dnsquery=dnsquery,
-        server=DNSUPDATE_SERVER,
-        creds=CREDS,
-
-    )
-except NotImplementedError:
-    print("Your GSSAPI implementation does not have support for manipulating credential stores.")
-    sys.exit(1)
-
-# Do cleanup first, before issuing our challenge
-if args.cleanup2 is not None:
-	old_challenges = dnslookup.get_txt(acme_challenge_name)
-	if len(old_challenges) == 0:
-		debug(f"No challenge records to clean up for {acme_challenge_name}")
-	for old_challenge in old_challenges:
-		# if old_challenge is not a tuple, make it a tuple
-		if isinstance(old_challenge, tuple):
-			old_challenge_tuple = old_challenge
+		# If the user provided a challenge, and it does _not_ match, then tell them.
+		if args.cleanup2 != cleanup_challenge:
+			print(f"You provided the wrong cleanup challenge for {args.name}.")
+			print(f"Change `--cleanup2 {args.cleanup2}` to `--cleanup` and try again.")
+			sys.exit(1)
 		else:
-			old_challenge_tuple = (old_challenge,)
+			# We got a valid challenge!
+			info('Will cleanup other ACME challenges')
 
-		# To ensure log messages print, make a tuple of strings.
-		old_challenge_str = tuple(
-			x.decode('ascii', 'backslashreplace')
-			for x in old_challenge_tuple
+	# Parse custom Kerberos credentials config.
+	CREDS: sudns01.clients.tkey.KrbCreds | None = None
+	if sudns01.clients.tkey.HAS_CREDENTIAL_STORE:
+		debug('We have the Credential Store Extensions')
+		if args.ccache is not None or args.keytab is not None:
+			CREDS = sudns01.clients.tkey.KrbCreds(
+				ccache=args.ccache,
+				client_keytab=args.keytab,
+			)
+
+	DNSUPDATE_SERVER: str = args.nsupdate
+	DNSUPDATE_PORT: int = args.port
+	DNSUPDATE_TIMEOUT: float = args.timeout
+	TARGET_NAME: dns.name.Name = dns.name.from_text(args.name)
+	TTL: int = 60
+	CHALLENGE: str = args.challenge
+	dns_queries_on_udp = args.udp
+
+	# Set up a Resolver
+	dnslookup = sudns01.clients.resolver.ResolverClient()
+
+	# From the target name (a FQDN), we need to work out:
+	# * The _acme-challenge FQDN
+	# * The underlying zone name
+	# * The _acme-challenge name relative to the zone name
+	target_domain = dnslookup.get_zone_name(TARGET_NAME)
+	ACME_CHALLENGE_LABEL = dns.name.Name(labels=('_acme-challenge',))
+	acme_challenge_name = ACME_CHALLENGE_LABEL.concatenate(TARGET_NAME)
+	acme_challenge_name_relative = acme_challenge_name.relativize(target_domain)
+	info(f"We will be working in domain {target_domain}")
+	info(f"We will be modifying label {acme_challenge_name_relative}")
+
+	# Get the IPs for our DNS server
+	try:
+		dnsupdate_server_ips = dnslookup.get_ip(DNSUPDATE_SERVER)
+	except sudns01.clients.exceptions.ResolverError as e:
+		print(f"Temporary error looking up {DNSUPDATE_SERVER}: {e}")
+		sys.exit(2)
+	except sudns01.clients.exceptions.ResolverErrorPermanent as e:
+		print(f"Permanent error looking up {DNSUPDATE_SERVER}: {e}")
+		sys.exit(1)
+	if len(dnsupdate_server_ips) == 0:
+		print(f"No IP addresses found for {DNSUPDATE_SERVER}")
+		sys.exit(1)
+
+	# Create the client for sending DNS queries
+	dnsquery = sudns01.clients.query.QueryClient(
+		ips=dnsupdate_server_ips,
+		port=DNSUPDATE_PORT,
+		timeout=DNSUPDATE_TIMEOUT,
+		udp=dns_queries_on_udp,
+	)
+
+	# Set up DNS signing
+	try:
+		signer = sudns01.clients.tkey.GSSTSig(
+			dnsquery=dnsquery,
+			server=DNSUPDATE_SERVER,
+			creds=CREDS,
+
 		)
-		info(f"Cleaning up old challenge {old_challenge_str}")
+	except NotImplementedError:
+		print("Your GSSAPI implementation does not have support for manipulating credential stores.")
+		sys.exit(1)
 
-		# Construct a TXT record to target for deletion
-		old_challenge_rdata = dns.rdtypes.ANY.TXT.TXT(
-			rdclass=dns.rdataclass.IN,
-			rdtype=dns.rdatatype.TXT,
-			strings=old_challenge_tuple,
-		)
+	# Do cleanup first, before issuing our challenge
+	if args.cleanup2 is not None:
+		old_challenges = dnslookup.get_txt(acme_challenge_name)
+		if len(old_challenges) == 0:
+			debug(f"No challenge records to clean up for {acme_challenge_name}")
+		for old_challenge in old_challenges:
+			# if old_challenge is not a tuple, make it a tuple
+			if isinstance(old_challenge, tuple):
+				old_challenge_tuple = old_challenge
+			else:
+				old_challenge_tuple = (old_challenge,)
 
-		# Construct our deletion request
-		old_challenge_delete = dns.update.UpdateMessage(
-			zone=target_domain,
-			rdclass=dns.rdataclass.IN,
-			**signer.dnspython_args,
-		)
-		old_challenge_delete.delete(
-			acme_challenge_name_relative,
-			old_challenge_rdata,
-		)
+			# To ensure log messages print, make a tuple of strings.
+			old_challenge_str = tuple(
+				x.decode('ascii', 'backslashreplace')
+				for x in old_challenge_tuple
+			)
+			info(f"Cleaning up old challenge {old_challenge_str}")
 
-		# Send out the request.  If we get an exception, log a warning, but
-		# otherwise continue.
-		try:
-			dns_delete_response = dnsquery.query(old_challenge_delete)
-		except sudns01.clients.exceptions.NoServers:
-			warn(f"Ran out of DNS servers to try, while trying to clean up {old_challenge_str}")
-		except sudns01.clients.exceptions.DNSError:
-			warn(f"DNS error - hopefully temporary, while trying to clean up {old_challenge_str}")
+			# Construct a TXT record to target for deletion
+			old_challenge_rdata = dns.rdtypes.ANY.TXT.TXT(
+				rdclass=dns.rdataclass.IN,
+				rdtype=dns.rdatatype.TXT,
+				strings=old_challenge_tuple,
+			)
 
-# Prepare our challenge record
-# Note that TXT records are tuples of byte strings, with no specific encoding.
-# Per RFC 8555 §8.4, challenge tokens only contain characters from the
-# base64url alphabet, which is a subset of ASCII.  So, we can encode as ASCII.
-challenge_rdata = dns.rdtypes.ANY.TXT.TXT(
-    rdclass=dns.rdataclass.IN,
-    rdtype=dns.rdatatype.TXT,
-    strings=(
-        CHALLENGE.encode('ASCII'),
-    ),
-)
+			# Construct our deletion request
+			old_challenge_delete = dns.update.UpdateMessage(
+				zone=target_domain,
+				rdclass=dns.rdataclass.IN,
+				**signer.dnspython_args,
+			)
+			old_challenge_delete.delete(
+				acme_challenge_name_relative,
+				old_challenge_rdata,
+			)
 
-# Add a new ACME Challenge record
+			# Send out the request.  If we get an exception, log a warning, but
+			# otherwise continue.
+			try:
+				dns_delete_response = dnsquery.query(old_challenge_delete)
+			except sudns01.clients.exceptions.NoServers:
+				warn(f"Ran out of DNS servers to try, while trying to clean up {old_challenge_str}")
+			except sudns01.clients.exceptions.DNSError:
+				warn(f"DNS error - hopefully temporary, while trying to clean up {old_challenge_str}")
 
-# Create the Add request
-challenge_add = dns.update.UpdateMessage(
-    zone=target_domain,
-    rdclass=dns.rdataclass.IN,
-    **signer.dnspython_args,
-)
-challenge_add.add(
-    acme_challenge_name_relative,
-    TTL,
-    challenge_rdata,
-)
+	# Prepare our challenge record
+	# Note that TXT records are tuples of byte strings, with no specific encoding.
+	# Per RFC 8555 §8.4, challenge tokens only contain characters from the
+	# base64url alphabet, which is a subset of ASCII.  So, we can encode as ASCII.
+	challenge_rdata = dns.rdtypes.ANY.TXT.TXT(
+		rdclass=dns.rdataclass.IN,
+		rdtype=dns.rdatatype.TXT,
+		strings=(
+			CHALLENGE.encode('ASCII'),
+		),
+	)
 
-# Send out the request
-try:
-    dns_add_response = dnsquery.query(challenge_add)
-except sudns01.clients.exceptions.NoServers:
-    print("Ran out of DNS servers to try")
-    sys.exit(1)
-except sudns01.clients.exceptions.DNSError:
-    print("DNS error - hopefully temporary!")
-    sys.exit(2)
+	# Add a new ACME Challenge record
 
-# Wait to do the deletion
-input('Press Return to delete the record')
+	# Create the Add request
+	challenge_add = dns.update.UpdateMessage(
+		zone=target_domain,
+		rdclass=dns.rdataclass.IN,
+		**signer.dnspython_args,
+	)
+	challenge_add.add(
+		acme_challenge_name_relative,
+		TTL,
+		challenge_rdata,
+	)
 
-# Remove the new ACME Challenge record
+	# Send out the request
+	try:
+		dns_add_response = dnsquery.query(challenge_add)
+	except sudns01.clients.exceptions.NoServers:
+		print("Ran out of DNS servers to try")
+		sys.exit(1)
+	except sudns01.clients.exceptions.DNSError:
+		print("DNS error - hopefully temporary!")
+		sys.exit(2)
 
-# Create the Delete request
-challenge_delete = dns.update.UpdateMessage(
-    zone=target_domain,
-    rdclass=dns.rdataclass.IN,
-    **signer.dnspython_args,
-)
-challenge_delete.delete(
-    acme_challenge_name_relative,
-    challenge_rdata,
-)
+	# Wait to do the deletion
+	input('Press Return to delete the record')
 
-# Send out the request
-try:
-    dns_delete_response = dnsquery.query(challenge_delete)
-except sudns01.clients.exceptions.NoServers:
-    print("Ran out of DNS servers to try")
-    sys.exit(1)
-except sudns01.clients.exceptions.DNSError:
-    print("DNS error - hopefully temporary!")
-    sys.exit(2)
+	# Remove the new ACME Challenge record
+
+	# Create the Delete request
+	challenge_delete = dns.update.UpdateMessage(
+		zone=target_domain,
+		rdclass=dns.rdataclass.IN,
+		**signer.dnspython_args,
+	)
+	challenge_delete.delete(
+		acme_challenge_name_relative,
+		challenge_rdata,
+	)
+
+	# Send out the request
+	try:
+		dns_delete_response = dnsquery.query(challenge_delete)
+	except sudns01.clients.exceptions.NoServers:
+		print("Ran out of DNS servers to try")
+		sys.exit(1)
+	except sudns01.clients.exceptions.DNSError:
+		print("DNS error - hopefully temporary!")
+		sys.exit(2)
+
+	# All done!
+	debug('Exiting successfully!')
+	sys.exit(0)
