87f65eddd31b5e3df3d023a17864ae4efebe02e3
A. Karl Kornel <akkornel@stanford.edu>
2025-03-25 20:13:20 -0700

    Add a module to handle cleanup
    This is a module which handles the work around cleaning up of old
    challenges.  It's intended to centralize code that's currently in
    `sudns01.cli`, and which would've had to be copied into
    `sudns01.certbot`.
    
    The code handles generating cleanup challenges, checking challenge
    validity, getting old challenges (via a DNS lookup), and creating the
    nsupdate delete message that is needed to delete a TXT record.
    
    The only thing this module doesn't do is to actually send out the
    challenge messages, though that could be added in the future (though
    testing it would be annoying).
    
    Some changes were made during refactoring:
    
    * DNS names are now brought in as `dns.name.Name` instances, not as
      strings.
    
    * USER-FACING: As a result of the above change, DNS names now end in a
      `.`, so the cleanup challenge code is different.
    
    One more note: Most methods allow specifying an ACME challenge domain
    name as a parameter.  If not provided, one is automatically derived.
    This is because certbot's code does the derivation for you.
    
    Finally, also included is test code for all of the above.

---
+++ sudns01/clients/challenge.py
@@ -0,0 +1,306 @@
+# vim: ts=4 sw=4 noet
+
+# Copyright 2025 The Board of Trustees of the Leland Stanford Junior University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Code for cleanup-related stuff
+
+This module contains code for cleaning up old challenges.
+"""
+
+# stdlib imports
+import binascii
+import collections.abc
+import dataclasses
+import logging
+from typing import NewType, NamedTuple
+
+# PyPi imports
+import dns.name
+import dns.rdataclass
+import dns.rdatatype
+import dns.rdtypes.ANY.TXT
+
+# Local imports
+import sudns01.clients.exceptions
+import sudns01.clients.resolver
+import sudns01.clients.tkey
+
+# Set up logging
+logger = logging.getLogger(__name__)
+exception = logger.exception
+error = logger.error
+warn = logger.warning
+info = logger.info
+debug = logger.debug
+
+
+ACME_CHALLENGE_LABEL = dns.name.Name(labels=('_acme-challenge',))
+
+SplitNameLabel = NewType('SplitNameLabel', dns.name.Name)
+SplitNameZone = NewType('SplitNameZone', dns.name.Name)
+class SplitName(NamedTuple):
+	label: SplitNameLabel
+	zone: SplitNameZone
+
+# Make a class for cleanup-related stuff
+@dataclasses.dataclass(frozen=True)
+class Cleanup():
+	"""Manage cleanup for a single domain
+
+	This handles the cleanup work for a single domain.  It handles generating
+	the cleanup challenge, doing the cleanup, fetching records to clean up, and
+	generating the cleanup messages.
+
+The work of signing and sending is handled separately.
+	"""
+
+	domain: dns.name.Name
+	"""The domain that may end up having records cleaned up.
+	"""
+
+	_acme_name: dns.name.Name | None = dataclasses.field(init=False)
+	"""Cached ACME name for domain.
+
+	This is a DNS name, formed by prepending the ACME label (`_acme-challenge`)
+	to the domain.
+
+	This is populated by the first call to `acme_name`.
+	"""
+
+	_split: SplitName | None = dataclasses.field(init=False)
+	"""Split label and zone name for our ACME name.
+
+	nsupdate messages require the FQDN be split into a zone part, and a label
+	part.  For example, for `_acme-challenge.blargh.stanford.edu.`, the zone is
+	`stanford.edu.` and the label is `_acme-challenge.blargh`.  This caches
+	those two parts, assuming `_acme_name` contains the FQDN.
+
+	This is populated by the first call to `split`.
+	"""
+
+	def __post_init__(self) -> None:
+		"""Populate internal class members (or make placeholders).
+		"""
+		# Set our two internal members to `None`
+		object.__setattr__(
+			self,
+			'_acme_name',
+			None,
+		)
+		object.__setattr__(
+			self,
+			'_split',
+			None,
+		)
+
+	@property
+	def acme_name(self) -> dns.name.Name:
+		"""Return the Name of the ACME challenge domain.
+		"""
+		# If we don't have the name generated yet, then generate and cache.
+		if self._acme_name is None:
+			result = ACME_CHALLENGE_LABEL.concatenate(self.domain)
+			object.__setattr__(
+				self,
+				'_acme_name',
+				result,
+			)
+			return result
+		else:
+			return self._acme_name
+
+	def split(
+		self,
+		resolver: sudns01.clients.resolver.ResolverClient,
+		acme_challenge_name: dns.name.Name | None = None,
+	) -> SplitName:
+		"""Split the ACME challenge name into separate label and zone parts.
+
+		When making nsupdate messages, instead of providing the name to update
+		as a FQDN, you must provide the zone name and a separate label name.
+
+		The zone name can be found by doing an SOA lookup.  For example, with
+		SOA lookups, you can discover that both `blargh.stanford.edu` and
+		`_acme-challenge.blargh.stanford.edu` are part of the same zone,
+		`stanford.edu`.  So in both cases, the zone is `stanford.edu.` and the
+		labels are `blargh` and `_acme-challenge.blargh`.
+
+		This method performs the lookup and the split.
+
+		:param resolver: A resolver to use for SOA record lookup.
+
+		:param acme_challenge_name: The DNS name to use for queries.  If not
+		provided, `_acme-challenge.` will be prepended to the existing
+		domain.
+
+		:returns: A tuple containing the label and zone portions of the domain.
+		"""
+		if acme_challenge_name is None:
+			acme_challenge_name = self.acme_name
+
+		# If the ACME challenge name is what we expect, can we use cache?
+		if acme_challenge_name == self.acme_name and self._split is not None:
+			return self._split
+
+		# Use the resolver to work out our zone name
+		zone_name = resolver.get_zone_name(
+			acme_challenge_name,
+			raise_on_cdname=False,
+		)
+
+		# Split our ACME name into relative and zone parts
+		acme_name_relative = acme_challenge_name.relativize(zone_name)
+		result = SplitName(
+			SplitNameLabel(acme_name_relative),
+			SplitNameZone(zone_name),
+		)
+
+		# Possibly cache, and return
+		if acme_challenge_name == self.acme_name:
+			object.__setattr__(
+				self,
+				'_split',
+				result,
+			)
+		return result
+
+	@property
+	def challenge(self) -> str:
+		"""Return the challenge string the user needs to provide, in order to do a cleanup.
+
+		If the user wants to do a cleanup of old TXT records, they must provide
+		a challenge string.  This returns the challenge string.
+
+		:returns: A string the user must provide, confirming the intent to clean up records.
+		"""
+		challenge_str = "{:#010x}".format(
+			binascii.crc32(str(self.domain).encode('UTF-8'))
+		)
+		return challenge_str
+
+	def is_challenge_valid(
+		self,
+		challenge: str,
+	) -> bool:
+		"""Return True if a provided string matches this domain's challenge.
+
+		If the user wants to do a cleanup of old TXT records, they must provide
+		a challenge string.  This checks if the provided challenge string is
+		correct.
+
+		:param challenge: The challenge to verify.
+
+		:returns: True if the provided challenge is correct; False otherwise.
+		"""
+		debug(f"Checking provided challenge {challenge} against {self.challenge}")
+		return (True if challenge == self.challenge else False)
+
+	def get_old_challenges(
+		self,
+		resolver: sudns01.clients.resolver.ResolverClient,
+		acme_challenge_name: dns.name.Name | None = None,
+	) -> collections.abc.Iterator[tuple[bytes, ...]]:
+		"""Iterate over old challenges to be cleaned up.
+
+		Using the provided resolver, query the ACME Challenge name for TXT
+		records.  Return all that are found.
+
+		NOTE: TXT records are encoded as tuples of byte strings; this code
+		maintains that structure.
+
+		:param resolver: The DNS resolver to use.
+
+		:param acme_challenge_name: The DNS name to use for queries.  If not
+		provided, `_acme-challenge.` will be prepended to the existing
+		domain.
+
+		:returns: Tuples of byte strings.
+		"""
+		if acme_challenge_name is None:
+			acme_challenge_name = self.acme_name
+		debug(f"Looking up old TXT records for {acme_challenge_name}")
+
+		# Make something to hold what we end up returning
+		results: set[tuple[bytes, ...]] = set()
+
+		# Do the lookup!
+		records = resolver.get_txt(
+			acme_challenge_name,
+			raise_on_cdname=False,
+		)
+		if len(records) == 0:
+			debug(f"No challenge records to clean up for {acme_challenge_name}")
+			return
+
+		# Go through each returned record & convert to tuple
+		for record in records:
+			record_tuple: tuple[bytes, ...]
+			if isinstance(record, tuple):
+				debug(f"Passing through record: {record!r}")
+				record_tuple = record
+			else:
+				debug(f"Converting to tuple: {record!r}")
+				record_tuple = (record,)
+			yield record_tuple
+
+		# All done!
+		return
+
+	def get_delete_message(
+		self,
+		record: tuple[bytes, ...],
+		resolver: sudns01.clients.resolver.ResolverClient,
+		signer: sudns01.clients.tkey.GSSTSig,
+		acme_challenge_name: dns.name.Name | None = None
+	) -> dns.update.UpdateMessage:
+		"""Get a delete-TXT-record message for a challenge.
+
+		:param record: The TXT record to delete.
+
+		:param resolver: A DNS resolver, to look up the domain's zone.
+
+		:param signer: A TSIG signer for the message.
+
+		:param acme_challenge_name: The DNS name to use for queries.  If not
+		provided, `_acme-challenge.` will be prepended to the existing
+		domain.
+
+		:returns: A ready-to-send DNS message.
+		"""
+		if acme_challenge_name is None:
+			acme_challenge_name = self.acme_name
+
+		# Split our domain into label and zone parts
+		domain_parts = self.split(resolver)
+
+		# Construct a TXT record to target for deletion
+		rdata = dns.rdtypes.ANY.TXT.TXT(
+			rdclass=dns.rdataclass.IN,
+			rdtype=dns.rdatatype.TXT,
+			strings=record,
+		)
+
+		# Construct our deletion request
+		message = dns.update.UpdateMessage(
+			zone=domain_parts.zone,
+			rdclass=dns.rdataclass.IN,
+			**signer.dnspython_args,
+		)
+		message.delete(
+			domain_parts.label,
+			rdata,
+		)
+
+		return message
---
+++ tests/test_challenge_cleanup.py
@@ -0,0 +1,284 @@
+# vim: ts=4 sw=4 noet
+
+# Copyright 2025 The Board of Trustees of the Leland Stanford Junior University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Test the code responsible for cleaning up other TXT records
+
+# Stdlib imports
+import collections.abc
+import dataclasses
+import os
+
+# PyPi imports
+import dns.name
+import dns.rdataclass
+import dns.rdatatype
+import dns.rdtypes.ANY.TXT
+import dns.update
+import pytest
+
+# Local imports
+import sudns01.clients.challenge
+import sudns01.clients.resolver
+import sudns01.clients.tkey
+
+
+# Make a couple of test cases
+@dataclasses.dataclass(frozen=True)
+class Case():
+	domain: dns.name.Name
+	challenge: str
+	acme_name: dns.name.Name
+cases: list[Case] = [
+	Case(
+		domain=dns.name.from_text('blargh.stanford.edu'),
+		challenge='0x95d402d5',
+		acme_name=dns.name.from_text('_acme-challenge.blargh.stanford.edu'),
+	),
+	Case(
+		domain=dns.name.from_text('example.com'),
+		challenge='0xbcdf1324',
+		acme_name=dns.name.from_text('_acme-challenge.example.com'),
+	),
+]
+
+# Make some fixtures
+@pytest.fixture
+def local_resolver() -> sudns01.clients.resolver.ResolverClient | None:
+	"""Set up a resolver client pointing to a local test DNS server.
+
+	This provides a `ResolverClient` that works against the local test DNS
+	server.  It is only provided if the `TEST_DNS_PORT` environment variable is
+	set.
+
+	If the environment variable is not set, then `None` is returned.
+	"""
+	if 'TEST_DNS_PORT' not in os.environ:
+		return None
+
+	local_resolver_resolver = dns.resolver.Resolver(configure=False)
+	local_resolver_resolver.nameservers = ['127.0.0.1']
+	local_resolver_resolver.port = int(os.environ['TEST_DNS_PORT'])
+	local_resolver_resolver.timeout = 0.1
+	local_resolver_resolver.lifetime = 0.1
+	local_resolver = sudns01.clients.resolver.ResolverClient()
+	local_resolver._resolver = local_resolver_resolver
+	local_resolver._resolver_nocache = local_resolver_resolver
+	return local_resolver
+
+@pytest.fixture
+def local_signer(
+	local_resolver,
+) -> collections.abc.Generator[sudns01.clients.tkey.GSSTSig | None, None, None]:
+	"""Set up a signer client using local Kerberos config.
+
+	This provides a GSSTSig instance that works against the local test KDC.
+	It is only provided if…
+
+	* `local_resolver` returns a ResolverClient; and
+
+	* The `KR5CONFIG` environment variable is set.
+
+	If any condition is not met, then `None` is returned.
+
+	If a GSSTSig instance *is* returned, then it will be properly closed once
+	these tests have completed.
+	"""
+	if 'KRB5_CONFIG' not in os.environ or local_resolver is None:
+		# Yield nothing, then return to skip any cleanup
+		yield None
+		return
+
+	# Create a query client & signer, then hand over the signer
+	local_dnsquery = sudns01.clients.query.QueryClient(
+		ips=['127.0.0.1'],
+		port=int(os.environ['TEST_DNS_PORT']),
+		timeout=0.1,
+	)
+	local_signer = sudns01.clients.tkey.GSSTSig(
+		dnsquery=local_dnsquery,
+		server='ns.localdomain',
+	)
+	yield local_signer
+
+	# At the end of testing, clean up the signer
+	local_signer.close()
+
+
+# Now, our tests
+def test_acme_name() -> None:
+	"""Make sure the ACME names are generated as expected.
+	"""
+
+	for case in cases:
+		cleanup = sudns01.clients.challenge.Cleanup(case.domain)
+		assert cleanup.acme_name == case.acme_name
+
+def test_split(local_resolver) -> None:
+	"""Make sure domain splitting works correctly
+	"""
+	if local_resolver is None:
+		pytest.skip('No Test DNS Server configured')
+
+	# In the zone file, blargh.localdomain should have two TXT entries
+	cleanup_blargh = sudns01.clients.challenge.Cleanup(
+		dns.name.from_text('blargh.localdomain'),
+	)
+
+	# Run split twice, with and without the ACME name
+	blargh_split1 = cleanup_blargh.split(
+		resolver=local_resolver,
+		acme_challenge_name=dns.name.from_text('_acme-challenge.blargh.localdomain'),
+	)
+	blargh_split2 = cleanup_blargh.split(
+		resolver=local_resolver,
+	)
+
+	# Check that both methods give the same result, and we got what we expected
+	assert blargh_split1 == blargh_split2
+	assert blargh_split1.label == dns.name.Name(
+		labels=('_acme-challenge', 'blargh')
+	)
+	assert blargh_split1.zone == dns.name.from_text('localdomain')
+
+def test_challenge() -> None:
+	"""Make sure challenge are checked correctly.
+
+	We test that the expected challenge string is generated, and that the
+	challenge-validity check works.
+	"""
+
+	for case in cases:
+		cleanup = sudns01.clients.challenge.Cleanup(case.domain)
+		assert cleanup.challenge == case.challenge
+		assert cleanup.is_challenge_valid(case.challenge) is True
+		assert cleanup.is_challenge_valid('0xdeadbeef') is False
+
+def test_get_old_challenges(
+	local_resolver
+) -> None:
+	"""Make sure old challenges are returned correctly.
+	"""
+	if local_resolver is None:
+		pytest.skip('No Test DNS Server configured')
+
+	# In the zone file, blargh.localdomain should have two TXT entries
+	cleanup_blargh = sudns01.clients.challenge.Cleanup(
+		dns.name.from_text('blargh.localdomain'),
+	)
+
+	# Run the check twice, with and without the ACME name
+	blargh_challenge_iterator1 = cleanup_blargh.get_old_challenges(
+		resolver=local_resolver,
+		acme_challenge_name=dns.name.from_text('_acme-challenge.blargh.localdomain'),
+	)
+	blargh_challenge_iterator2 = cleanup_blargh.get_old_challenges(
+		resolver=local_resolver,
+	)
+	blargh_challenges1 = set([x for x in blargh_challenge_iterator1])
+	blargh_challenges2 = set([x for x in blargh_challenge_iterator2])
+
+	# Make sure the two checks returned the same results (2 entries)
+	assert blargh_challenges1 == blargh_challenges2
+	assert len(blargh_challenges1) == 2
+
+	# Check that we got the expected entries
+	expected_tuple1 = (b'single entry',)
+	expected_tuple2 = (b'entry one', b'entry two')
+	assert expected_tuple1 in blargh_challenges1
+	assert expected_tuple2 in blargh_challenges1
+
+	# Finally, check a record that has no old challenges
+	cleanup_ns = sudns01.clients.challenge.Cleanup(
+		dns.name.from_text('ns.localdomain'),
+	)
+	ns_challenge_iterator = cleanup_ns.get_old_challenges(
+		resolver=local_resolver,
+	)
+	ns_challenges = set([x for x in ns_challenge_iterator])
+	assert len(ns_challenges) == 0
+
+def test_get_delete_message(
+	local_resolver,
+	local_signer,
+) -> None:
+	"""Make sure we get the delete messages we expect.
+	"""
+	if local_resolver is None:
+		pytest.skip('No Test DNS Server configured')
+	if local_signer is None:
+		pytest.skip('No Test KDC configured')
+
+	# Make some DNS name components
+	acme_challenge = dns.name.Name(labels=('_acme-challenge',))
+	blargh = dns.name.Name(labels=('blargh',))
+	localdomain = dns.name.from_text('localdomain')
+
+	acme_challenge_blargh = acme_challenge + blargh
+	acme_challenge_blargh_localdomain = acme_challenge_blargh + localdomain
+	blargh_localdomain = blargh + localdomain
+
+	# In the zone file, blargh.localdomain should have two TXT entries
+	cleanup = sudns01.clients.challenge.Cleanup(
+		blargh_localdomain,
+	)
+
+	# Go through each challenge, make a message, and check it
+	challenge_iterator = cleanup.get_old_challenges(
+		resolver=local_resolver,
+		acme_challenge_name=acme_challenge_blargh_localdomain,
+	)
+	for challenge in challenge_iterator:
+		# Generate a delete message, with and without an explicit ACME
+		# challenge name.
+		message1 = cleanup.get_delete_message(
+			record=challenge,
+			resolver=local_resolver,
+			signer=local_signer,
+			acme_challenge_name=acme_challenge_blargh_localdomain,
+		)
+		message2 = cleanup.get_delete_message(
+			record=challenge,
+			resolver=local_resolver,
+			signer=local_signer,
+		)
+
+		# Check message class and top-level fields
+		assert isinstance(message1, dns.update.UpdateMessage)
+		assert len(message1.zone) == 1
+		assert len(message1.update) == 1
+		assert isinstance(message2, dns.update.UpdateMessage)
+		assert len(message2.zone) == 1
+		assert len(message2.update) == 1
+
+		# Check zone
+		zone = message1.zone[0]
+		assert zone == message2.zone[0]
+		assert zone.name == localdomain
+
+		# Check update
+		update = message1.update[0]
+		assert update == message2.update[0]
+		assert update.name == acme_challenge_blargh
+		assert update.rdclass == dns.rdataclass.IN
+		assert update.rdtype == dns.rdatatype.TXT
+		assert update.deleting == dns.rdataclass.NONE
+		assert len(update) == 1
+
+		# Check update rdata
+		rdata = update[0]
+		assert rdata.rdclass == dns.rdataclass.IN
+		assert rdata.rdtype == dns.rdatatype.TXT
+		assert rdata.strings == challenge
